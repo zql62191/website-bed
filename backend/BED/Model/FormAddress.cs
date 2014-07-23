d824ba95784065919f4f658fa1515c8ac8568698
Devin Marsh <dmarsh@cdmprinceton.com>
2014-07-23 18:30:43 +0000

    Forgot to add new classes to repo

---
+++ backend/BED/Model/FormAddress.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace BEDService.Model
+{
+    [DataContract]
+    public class FormAddress
+    {
+        [DataMember(IsRequired = true)]
+        public string FName { get; set; }
+        [DataMember(IsRequired = true)]
+        public string MName { get; set; }
+        [DataMember(IsRequired = true)]
+        public string LName { get; set; }
+        [DataMember(IsRequired = true)]
+        public string Address1 { get; set; }
+        [DataMember(IsRequired = false)]
+        public string Address2 { get; set; }
+        [DataMember(IsRequired = true)]
+        public string City { get; set; }
+        [DataMember(IsRequired = true)]
+        public string State { get; set; }
+        [DataMember(IsRequired = true)]
+        public string Zip { get; set; }
+    }
+}
