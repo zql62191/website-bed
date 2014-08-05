a09c537a89177f0eb3e6c982ea1f05e598b283db
Maneesha Seth <rseth@cdmprinceton.com>
2014-08-05 20:42:44 +0000

    Fixed ExitMCC answer code

--- backend/BEDUnsubscribe/App_Code/BEDUnsubscribe.svc.cs
+++ backend/BEDUnsubscribe/App_Code/BEDUnsubscribe.svc.cs
@@ -61,7 +61,7 @@ namespace BEDService
                     regMgr.Individual = hcp;
 
                     List<QuestionResponse> questionResponses = new List<QuestionResponse>();
-                    QuestionResponse questionResponse = new QuestionResponse(Int32.Parse(ConfigurationManager.AppSettings["RTIDExitMCC"]), Int32.Parse(ConfigurationManager.AppSettings["RTIDAnsOpen"]), ConfigurationManager.AppSettings["MCCUnsubscribe"]);
+                    QuestionResponse questionResponse = new QuestionResponse(Int32.Parse(ConfigurationManager.AppSettings["RTIDExitMCC"]),  Int32.Parse(ConfigurationManager.AppSettings["MCCUnsubscribe"]));
                     questionResponses.Add(questionResponse);
 
                     questionResponse = new QuestionResponse(Int32.Parse(ConfigurationManager.AppSettings["RTIDOptOutBEDEmail"]), Int32.Parse(ConfigurationManager.AppSettings["RTIDAnsYes"]));
@@ -152,7 +152,7 @@ namespace BEDService
                     regMgr.Individual = hcp;
 
                     List<QuestionResponse> questionResponses = new List<QuestionResponse>();
-                    QuestionResponse questionResponse = new QuestionResponse(Int32.Parse(ConfigurationManager.AppSettings["RTIDExitMCC"]), Int32.Parse(ConfigurationManager.AppSettings["RTIDAnsOpen"]), ConfigurationManager.AppSettings["MCCUnsubscribe"]);
+                    QuestionResponse questionResponse = new QuestionResponse(Int32.Parse(ConfigurationManager.AppSettings["RTIDExitMCC"]), Int32.Parse(ConfigurationManager.AppSettings["MCCUnsubscribe"]));
                     questionResponses.Add(questionResponse);
 
                     questionResponse = new QuestionResponse(Int32.Parse(ConfigurationManager.AppSettings["RTIDOptOutBEDDirectMail"]), Int32.Parse(ConfigurationManager.AppSettings["RTIDAnsYes"]), ConfigurationManager.AppSettings["MCCUnsubscribe"]);
@@ -242,7 +242,7 @@ namespace BEDService
                     regMgr.Individual = hcp;
 
                     List<QuestionResponse> questionResponses = new List<QuestionResponse>();
-                    QuestionResponse questionResponse = new QuestionResponse(Int32.Parse(ConfigurationManager.AppSettings["RTIDExitMCC"]), Int32.Parse(ConfigurationManager.AppSettings["RTIDAnsOpen"]), ConfigurationManager.AppSettings["MCCUnsubscribe"]);
+                    QuestionResponse questionResponse = new QuestionResponse(Int32.Parse(ConfigurationManager.AppSettings["RTIDExitMCC"]), Int32.Parse(ConfigurationManager.AppSettings["MCCUnsubscribe"]));
                     questionResponses.Add(questionResponse);
 
                     questionResponse = new QuestionResponse(Int32.Parse(ConfigurationManager.AppSettings["RTIDOptOutAll"]), Int32.Parse(ConfigurationManager.AppSettings["RTIDAnsYes"]));
