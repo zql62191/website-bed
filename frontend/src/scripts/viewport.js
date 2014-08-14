fca9a1a8e5d77e85f2896028c91c138b795c139e
Ryan Suleski <rsuleski@cdmprinceton.com>
2014-08-14 20:20:20 +0000

    fixing iPad size issue [deploy: dotnet]

--- frontend/src/scripts/viewport.js
+++ frontend/src/scripts/viewport.js
@@ -1,5 +1,5 @@
 (function() {
     var vp = document.getElementById('viewport');
-    var width = (!/tablet/i.test(navigator.userAgent) && /[^-]mobi/i.test(navigator.userAgent)) ? '640' : '1024';
+    var width = (!/tablet|iPad/i.test(navigator.userAgent) && /[^-]mobi/i.test(navigator.userAgent)) ? '640' : '1024';
     vp.setAttribute('content', 'target-densitydpi=device-dpi, width=' + width + ', user-scalable=0');
 })();
