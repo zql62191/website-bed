eb6ead7c803b3bc3432d0cc95f3d6c60bd7264b2
Devin Marsh <dmarsh@cdmprinceton.com>
2014-12-01 18:30:44 +0000

    Fixed an issue with fonts on the sign-up form

--- frontend/dist/js/viewport.js
+++ frontend/dist/js/viewport.js
@@ -1,1 +1,5 @@
-!function(){var a=document.getElementById("viewport"),b=!/tablet|iPad/i.test(navigator.userAgent)&&/[^-]mobi/i.test(navigator.userAgent)?"640":"1024";a.setAttribute("content","target-densitydpi=device-dpi, width="+b+", user-scalable=0")}();
+(function() {
+    var vp = document.getElementById('viewport');
+    var width = (!/tablet|iPad/i.test(navigator.userAgent) && /[^-]mobi/i.test(navigator.userAgent)) ? '640' : '1024';
+    vp.setAttribute('content', 'target-densitydpi=device-dpi, width=' + width + ', user-scalable=0');
+})();
