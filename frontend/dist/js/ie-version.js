706f2fd8c1cefbcf0ec66df059a316d7efe94479
Devin Marsh <dmarsh@cdmprinceton.com>
2015-01-06 17:30:29 +0000

    Frontend checkbox alignment fix

--- frontend/dist/js/ie-version.js
+++ frontend/dist/js/ie-version.js
@@ -1,1 +1,25 @@
-!function(a){if(/msie|trident/i.test(a)){for(var b=function(){var b=a.match(/(?:msie |rv:)(\d+(\.\d+)?)/i),c=b&&b.length>1&&b[1]||"";return parseInt(c,10)}(),c="ie",d=6,e=12,f=["eq-ie"],g=d;e>=g;g++)b>g?f.push("gt-"+c+g):g>b?f.push("lt-"+c+g):b===g&&(f.push("lte-"+c+g),f.push("eq-"+c+g),f.push("gte-"+c+g));document.documentElement.className+=" "+f.join(" ")}}(navigator.userAgent||"");
+(function(ua) {
+    if (/msie|trident/i.test(ua)) {
+        var version = (function() {
+            var match = ua.match(/(?:msie |rv:)(\d+(\.\d+)?)/i);
+            var version = (match && match.length > 1 && match[1]) || '';
+            return parseInt(version, 10);
+        })();
+        var key = 'ie';
+        var min = 6;
+        var max = 12;
+        var klass = ['eq-ie'];
+        for (var v = min; v <= max; v++) {
+            if (version > v) {
+                klass.push('gt-' + key + v);
+            } else if (version < v) {
+                klass.push('lt-' + key + v);
+            } else if (version === v) {
+                klass.push('lte-' + key + v);
+                klass.push('eq-' + key + v);
+                klass.push('gte-' + key + v);
+            }
+        }
+        document.documentElement.className += ' ' + klass.join(' ');
+    }
+})(navigator.userAgent || '');
