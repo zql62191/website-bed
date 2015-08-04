895fe75cbe9e0fece52c8dce9136faec2ab0a830
msorensen <msorensen@cdmprinceton.com>
2015-08-04 16:01:02 -0400

    Added script to base.jade and to grunt build to add ASPX tags to beginning of files when running grunt devIntegrate.  This is for back end integration

--- frontend/tasks/options/jade.js
+++ frontend/tasks/options/jade.js
@@ -24,11 +24,51 @@ module.exports = {
             data: function(dest, src) {
                 var yfm = require('assemble-yaml');
                 return {
+                    aspx: false,
                     dev: true,
+                    devIntegrate: false,
                     prod: false,
                     from: src,
                     to: dest,
                     fileName: src[0].replace('src/markup/pages/', '').replace('.jade', '').split('/').pop(),
+                    fileString: src[0].replace('src/markup/pages/', '').replace('.jade', '').split('/').pop().replace('-', ''),
+                    site: yfm.extractJSON('src/markup/data/site.yaml'),
+                    env: yfm.extractJSON('src/markup/data/dev.yaml'),
+                    page: yfm.extractJSON('./' + src)
+                };
+            }
+        },
+        files: [{
+            cwd: 'src/markup/pages',
+            dest: 'dist/',
+            src: ['**/*.jade'],
+            expand: true,
+            filter: 'isFile',
+            ext: '.aspx'
+        }, {
+            cwd: 'src/markup/views',
+            dest: 'dist/views',
+            src: ['**/*.jade'],
+            expand: true,
+            filter: 'isFile',
+            ext: '.html'
+        }]
+    },
+    // same as dev but will trigger addition of aspx tags in jade base template
+    devIntegrate: {
+        options: {
+            pretty: true,
+            debug: false,
+            data: function(dest, src) {
+                var yfm = require('assemble-yaml');
+                return {
+                    aspx: true,
+                    dev: false,
+                    prod: false,
+                    from: src,
+                    to: dest,
+                    fileName: src[0].replace('src/markup/pages/', '').replace('.jade', '').split('/').pop(),
+                    fileString: src[0].replace('src/markup/pages/', '').replace('.jade', '').split('/').pop().replace('-', ''),
                     site: yfm.extractJSON('src/markup/data/site.yaml'),
                     env: yfm.extractJSON('src/markup/data/dev.yaml'),
                     page: yfm.extractJSON('./' + src)
@@ -58,11 +98,13 @@ module.exports = {
             data: function(dest, src) {
                 var yfm = require('assemble-yaml');
                 return {
+                    aspx: true,
                     dev: false,
                     prod: true,
                     from: src,
                     to: dest,
                     fileName: src[0].replace('src/markup/pages/', '').replace('.jade', '').split('/').pop(),
+                    fileString: src[0].replace('src/markup/pages/', '').replace('.jade', '').split('/').pop().replace('-', ''),
                     site: yfm.extractJSON('src/markup/data/site.yaml'),
                     env: yfm.extractJSON('src/markup/data/prod.yaml'),
                     page: yfm.extractJSON('./' + src)
