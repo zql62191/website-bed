990476f09ca181ad6a8e17977a2a1bb48b06e675
Christina Jiao <cjiao@cdmprinceton.com>
2015-08-05 14:04:56 -0400

    update sign-up.jade

--- frontend/src/scripts/app/controllers/signupController.js
+++ frontend/src/scripts/app/controllers/signupController.js
@@ -12,11 +12,6 @@
                 $scope.submitted = true;
                 $scope.$broadcast('show-errors-check-validity');
 
-                //$scope.optIn.firstName.$dirty = true;
-                //$scope.optIn.lastName.$dirty = true;
-                //$scope.optIn.email.$dirty = true;
-                //$scope.optIn.confirmEmail.$dirty = true;
-
                 if (!$scope.optIn.$invalid) {
                     if($scope.user.email == $scope.user.confirmEmail){
                         $scope.notMatch = false;
@@ -35,11 +30,6 @@
                     //alert("Please address the errors above!")
                 }
             };
-
-            //$scope.allowValidation = function () {
-            //    $scope.$broadcast('kickOffValidations');
-            //}
-
         }])
 
         .directive('showErrors', function (showErrorsConfig) {
@@ -52,31 +42,6 @@
                 }
                 return showSuccess;
             };
-
-            //var EMAIL_REGX = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
-            //return {
-            //    restrict: 'A',
-            //    require: 'ngModel',
-            //    link: function(scope, elm, attr, ctrl) {
-            //
-            //        if (attr.type === 'radio' || attr.type === 'checkbox') return;
-            //        elm.unbind('input').unbind('keydown').unbind('change');
-            //
-            //        elm.bind('blur', function () {
-            //            scope.$apply(dovalidation);
-            //        });
-            //        scope.$on('kickOffValidations', dovalidation)
-            //
-            //        function dovalidation() {
-            //            if (EMAIL_REGX.test(elm.val())) {
-            //                ctrl.$setValidity('email', true);
-            //            } else {
-            //                ctrl.$setValidity('email', false);
-            //            }
-            //        }
-            //    }
-            //};
-
         })
 
         .provider('showErrorsConfig', function () {
@@ -92,3 +57,16 @@
 
 
 }).call(this);
+
+
+//app.controller('signupController',
+//    function($scope) {
+//        $scope.submitted = false; 
+//        $scope.signupForm = function() { 
+//            if ($scope.optIn.$valid) { 
+//            // Submit as normal 
+//            } else { 
+//                $scope.optIn.submitted = true; 
+//            } 
+//        }
+//    });
