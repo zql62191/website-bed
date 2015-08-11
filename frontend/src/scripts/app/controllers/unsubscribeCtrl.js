394cba739c5d2e7da6f852acb2db0a87a01b3fe7
Zach Lin <zlin@sscg107910.sscg.local>
2015-08-11 15:23:10 -0400

    fixed the unsubscribe page and sign up page issues

--- frontend/src/scripts/app/controllers/unsubscribeCtrl.js
+++ frontend/src/scripts/app/controllers/unsubscribeCtrl.js
@@ -10,28 +10,12 @@
 
             $scope.submitForm = function() {
                 $scope.submitted = true;
-                $scope.$broadcast('show-errors-check-validity');
-
 
                 if (!$scope.optOutEmail.$invalid) {
                     console.log("user submit");
                     window.location.href="/hcp/thank-you-request.aspx";
 
                 }
-            };
-
-
-            $scope.syncFields = function(elem){
-                //sync form field input across all 3 forms if a similarly-named field exists
-                //called from any form field's blur? keyup? something?
-
-                console.log("HELLO FRIENDS!");
-
-                var elem = angular.element(elem.currentTarget);
-
-                //check if elem's name attribute is in any other form on the page
-                var otherFields = angular.element( "input[name=" + elem.prop('name') + "]" );
-
             };
 
         }])
@@ -42,7 +26,7 @@
 
             $scope.submitForm = function() {
                 $scope.submitted = true;
-                $scope.$broadcast('show-errors-check-validity');
+                //$scope.$broadcast('show-errors-check-validity');
 
                 if (!$scope.optoutdirectmail.$invalid) {
                     console.log("user submit");
@@ -55,9 +39,6 @@
 
             };
 
-            $scope.syncFields = function(elem){
-                emailController.syncFields(elem);
-            };
         }])
 
 
@@ -67,7 +48,7 @@
 
             $scope.submitForm = function() {
                 $scope.submitted = true;
-                $scope.$broadcast('show-errors-check-validity');
+                //$scope.$broadcast('show-errors-check-validity');
 
                 if (!$scope.optinall.$invalid) {
                     console.log("user submit");
@@ -80,34 +61,8 @@
 
             };
 
-            $scope.syncFields = function(elem){
-                emailController.syncFields(elem);
-            };
         }])
-        
-
-        .directive('showErrors', function (showErrorsConfig) {
-            var getShowSuccess;
-            getShowSuccess = function (options) {
-                var showSuccess;
-                showSuccess = showErrorsConfig.showSuccess;
-                if (options && options.showSuccess != null) {
-                    showSuccess = options.showSuccess;
-                }
-                return showSuccess;
-            };
-        })
-
-        .provider('showErrorsConfig', function () {
-            var _showSuccess;
-            _showSuccess = false;
-            this.showSuccess = function (showSuccess) {
-                return _showSuccess = showSuccess;
-            };
-            this.$get = function () {
-                return { showSuccess: _showSuccess };
-            };
-        })    
+       
 
     //.controller('clickController', ['$scope', function($scope){
     //    $scope.clickToTY = function(){
@@ -118,4 +73,5 @@
 
 
 }).call(this);
- 
+
+
